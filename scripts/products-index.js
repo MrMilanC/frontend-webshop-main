559d81562f843e62a62cfc3a06c853a85183c010
MrMilanC <69223405+MrMilanC@users.noreply.github.com>
2023-09-30 18:30:33 +0200

    Version 1.2

---
+++ scripts/products-index.js
@@ -0,0 +1,55 @@
+function displayProducts(data) {
+    console.log(data)
+    var productTableBodyIndex = $("#productTableBodyIndex");
+    productTableBodyIndex.empty(); // Clear existing rows
+
+    data.forEach(function (product) {
+        var row = "<tr>";
+        row += "<td>" + product.productId + "</td>";
+        row += "<td>" + product.productName + "</td>";
+        row += "<td>" + product.description + "</td>";
+        row += "<td>$" + product.price.toFixed(2) + "</td>";
+        row += "<td>" + product.category + "</td>";
+        row += "<td>" + product.quantity + "</td>";
+        row += '<td><img src="/static/img/' + product.imageName + '" height="100px" width="100px" style="border:5px solid black"></td>';
+        row += "</tr>";
+
+        var showProduct = $('<td>').append(
+            $('<a>').attr('href', '#' + product.id)
+                .addClass('btn btn-success')
+                .text('Show')
+                .click(function (e) {
+                    e.preventDefault();
+                    var productId = product.productId;
+                    $.ajax({
+                        url: "http://localhost:8080/admin/products/view/" + productId,
+                        method: "GET",
+                        dataType: "json",
+                        success: function (data) {
+                            console.log("Show product", data);
+                            sessionStorage.setItem('productId', productId);
+                            window.location.href = "product-detail.html";
+                        },
+                        error: function (xhr, status, error) {
+                            console.error(error);
+                        }
+                    });
+                })
+        );
+        productTableBodyIndex.append(row, showProduct);
+    });
+}
+
+$(document).ready(function () {
+        $.ajax({
+            url: "http://localhost:8080/products/view",
+            method: "GET",
+            dataType: "json",
+            success: function (data) {
+                displayProducts(data)
+            },
+            error: function (xhr, status, error) {
+                console.error(error);
+            }
+        });
+});
--- scripts/register.js
+++ scripts/register.js
@@ -1,31 +1,3 @@
-/*$(document).ready(function () {
-
-    var authToken = sessionStorage.getItem("token");
-    console.log("AuthToken:", authToken);
-
-    // Function to display users in the table
-    if (authToken) {
-        $.ajax({
-            url: "http://localhost:8080/register",
-            method: "POST",
-            dataType: "json",
-            headers: {
-                Authorization: "Bearer " + authToken
-            },
-            //cors: true,
-            success: function (data) {
-                displayUsers(data)
-            },
-            error: function (xhr, status, error) {
-                console.error(error);
-            }
-        });
-    } else {
-        // Handle the case where authToken is not found in sessionStorage
-        console.error("Authentication token not found in sessionStorage.");
-    }
-});*/
-
 console.log("Script loaded");
 $(document).ready(function () {
     $("#registerButton").click(function (event) {
@@ -68,45 +40,5 @@ $(document).ready(function () {
     });
 });
 
-/*
-console.log("Script loaded");
-$(document).ready(function () {
-    $("#registerButton").on("click", _e => {
-        console.log("Button clicked");
-        $(".input-error").removeClass("input-error");
-        $(".error-message").remove();
 
-        const newUser = {
-            "firstName": $("#firstName").val(),
-            "lastName": $("#lastName").val(),
-            "userName": $("#userName").val(),
-            "password": $("#password").val(),
-            "email": $("#email").val(),
-        }
-
-        $.ajax({
-            url: "http://localhost:8080/register/create",
-            type: "POST",
-            /*cors: true,
-            headers: {
-                Authorization: "Bearer " + authToken
-            },
-            contentType: "application/json",
-            data: JSON.stringify(newUser),
-            success: console.log,
-            error: error => {
-                console.log(error);
-                if (error.status === 400) {
-                    for (let err of error.responseJSON.errors) {
-                        const input = $("#" + err.field + "Input");
-                        input.addClass("input-error");
-                        const parent = input.parent();
-                        parent.append(`<p class="error-message">${err.defaultMessage}</p>`);
-                    }
-                    handleErrors(error.responseJSON.errors);
-                }
-            }
-        });
-    })
-});*/
 
