a20878127eca6e0f4c66ad1c6c65eed36911b2f5
MrMilanC <69223405+MrMilanC@users.noreply.github.com>
2023-10-11 22:15:59 +0200

    Meilenstein 1.09 View Cart

--- scripts/shopping-cart.js
+++ scripts/shopping-cart.js
@@ -1,6 +1,8 @@
-function displayProductsInCart(data) {
+function displayProductsShoppingList(data) {
     var productTableBodyCart = $("#productTableBodyCart");
     productTableBodyCart.empty(); // Clear existing rows
+
+    var userName = sessionStorage.getItem("usernameTokenCart");
 
     data.forEach(function (product) {
         var row = "<tr>";
@@ -39,18 +41,22 @@ function displayProductsInCart(data) {
         var cartProduct = $('<td>').append(
             $('<a>').attr('href', '#' + product.id)
                 .addClass('btn btn-success')
-                .text('Warenkorb')
+                .text('Kaufen')
                 .click(function (e) {
                     e.preventDefault();
                     var productId = product.productId;
                     $.ajax({
-                        url: "http://localhost:8080/products/view/" + productId,
-                        method: "GET",
-                        dataType: "json",
+                        url: "http://localhost:8080/cart/add/" + productId,
+                        method: "POST",
+                        data: {userName : userName},
+                        //data: JSON.stringify(sessionStorage.getItem('usernameTokenCart')),
+                        //dataType: "application/json",
+                        //processData: false,
+                        //contentType: false,
                         success: function (data) {
-                            console.log("Show product", data);
-                            sessionStorage.setItem('productId', productId);
-                            window.location.href = "product-detail.html";
+                            // console.log("Show product", data);
+                            // sessionStorage.setItem('productId', productId);
+                            // window.location.href = "product-detail.html";
                         },
                         error: function (xhr, status, error) {
                             console.error(error);
@@ -70,7 +76,7 @@ $(document).ready(function () {
     var tokens = authToken.split(".");
     var payload = JSON.parse(atob(tokens[1])); // Parse the token payload as JSON
     var username = payload.username; // Extract the username from the payload
-    sessionStorage.setItem('usernameToken', username);
+    sessionStorage.setItem('usernameTokenCart', username);
 
     // Check if a username exists
     // if (username) {
@@ -93,7 +99,7 @@ $(document).ready(function () {
             },
             //cors: true,
             success: function (data) {
-                displayProductsInCart(data)
+                displayProductsShoppingList(data)
             },
             error: function (xhr, status, error) {
                 console.error(error);
@@ -105,30 +111,111 @@ $(document).ready(function () {
     }
 });
 
+//////////////////////
+// WARENKORB HOLEN
+//////////////////////
+/*
+function displayProductsInCart(data) {
+    var productTableBodyBought = $("#productTableBodyBought");
+    productTableBodyBought.empty(); // Clear existing rows
 
+    data.forEach(function (product) {
+        var row = "<tr>";
+        row += "<td>" + product.productId + "</td>";
+        row += "<td>" + product.productName + "</td>";
+        row += "<td>" + product.description + "</td>";
+        row += "<td>$" + product.price.toFixed(2) + "</td>";
+        row += "<td>" + product.category.categoryId + "</td>";
+        row += "<td>" + product.quantity + "</td>";
+        row += '<td><img src="/frontend-webshop-main/img/user-files/' + product.imageName + '" height="100px" width="100px" style="border:5px solid black"></td>';
+        row += "</tr>";
 
-// // A click event listener to the "Admin-Zone" link
-// document.getElementById("admin-link").addEventListener("click", function () {
-//     var tempToken = sessionStorage.getItem("token");
-//     var authToken = tempToken.split(".");
-//     var payload = JSON.parse(atob(authToken[1])); // Parse the token payload as JSON
-//
-//     var username = payload.username; // Extract the username from the payload
-//
-//     // Check if a username exists
-//     if (username) {
-//         alert("Welcome, " + username + "!");
-//         console.log("Username: " + username);
-//     } else {
-//         alert("Sorry, no username found!");
-//         console.log("No username found");
-//     }
-// });
+        var showProduct = $('<td>').append(
+            $('<a>').attr('href', '#' + product.id)
+                .addClass('btn btn-success')
+                .text('Show')
+                .click(function (e) {
+                    e.preventDefault();
+                    var productId = product.productId;
+                    $.ajax({
+                        url: "http://localhost:8080/products/view/" + productId,
+                        method: "GET",
+                        dataType: "json",
+                        success: function (data) {
+                            console.log("Show product", data);
+                            sessionStorage.setItem('productId', productId);
+                            window.location.href = "product-detail.html";
+                        },
+                        error: function (xhr, status, error) {
+                            console.error(error);
+                        }
+                    });
+                })
+        );
 
+        var cartProduct = $('<td>').append(
+            $('<a>').attr('href', '#' + product.id)
+                .addClass('btn btn-success')
+                .text('entfernen')
+                .click(function (e) {
+                    e.preventDefault();
+                    var productId = product.productId;
+                    $.ajax({
+                        url: "http://localhost:8080/cart/remove/" + productId,
+                        method: "GET",
+                        dataType: "json",
+                        success: function (data) {
+                            // console.log("Show product", data);
+                            // sessionStorage.setItem('productId', productId);
+                            // window.location.href = "product-detail.html";
+                        },
+                        error: function (xhr, status, error) {
+                            console.error(error);
+                        }
+                    });
+                })
+        );
+        productTableBodyBought.append(row, showProduct, cartProduct);
+    });
+}
 
+$(document).ready(function () {
 
+    var authToken = sessionStorage.getItem("token");
+    console.log("AuthToken:", authToken);
 
+    var tokens = authToken.split(".");
+    var payload = JSON.parse(atob(tokens[1])); // Parse the token payload as JSON
+    var username = payload.username; // Extract the username from the payload
+    sessionStorage.setItem('usernameTokenCart', username);
 
+    // Function to display products in the table
+    if (authToken) {
+        $.ajax({
+            url: "http://localhost:8080/cart/view/" + username,
+            method: "GET",
+            dataType: "json",
+            headers: {
+                Authorization: "Bearer " + authToken
+            },
+            //cors: true,
+            success: function (data) {
+                displayProductsInCart(data)
+            },
+            error: function (xhr, status, error) {
+                console.error(error);
+            }
+        });
+    } else {
+        // Handle the case where authToken is not found in sessionStorage
+        console.error("Authentication token not found in sessionStorage.");
+    }
+});
+*/
+
+//////////////////////
+// ANZEIGE LOGIK
+//////////////////////
 
 /* get cart total from session on load */
 updateCartTotal();
